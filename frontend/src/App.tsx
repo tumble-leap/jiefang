30443e4fe64ac5de7bf42ca8a4fe86a038c9c8e8
陈博文 <mnsd@192.168.10.8>
2024-09-23 00:33:25 +0800

    good

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,37 +2,45 @@ import { useState, useEffect } from 'react';
 import pic from './assets/images/run.gif';
 import './App.css';
 
-const days: number[] = [6, 7, 8, 9, 12, 13, 14, 15, 16, 20, 21, 22, 23, 26, 27, 28, 29];
+const days: string[] = ['8.30','9.21', '9.22', '9.23', '9.26', '9.27', '9.28', '9.29'];
 
 function calcLeftWorkDays(): number {
     let count = 0;
     const today = new Date().getDate(); // 获取今天的日期（天数）
 
     for (const day of days) {
-        if (day >= today) {
+        const month = parseInt(day.split('.')[0]); // 获取月份
+        const dayNumber = parseInt(day.split('.')[1]); // 获取日期（天数）
+        if (month > new Date().getMonth()+1) {
             count++;
+        } else if (month < new Date().getMonth()+1) {
+            continue;
+        } else if (dayNumber > today) {
+            count++;
+        } else if (dayNumber === today) {
+            if (new Date().getHours() < 21) {
+                count++;
+            }
         }
     }
 
     return count;
 }
 
-// 目标时间 2024 年 9 月 30 日 21:00:00
 const targetDate: Date = new Date('2024-09-29T21:00:00');
 
 function calcLeftDays(): [string, boolean] {
     const now: Date = new Date();
     const timeDifference: number = targetDate.getTime() - now.getTime();
     if (timeDifference <= 0) {
-        return ["", false]; // 返回剩余天数为 0
+        return ["", true];
     }
 
-    // 计算天、小时、分钟、秒
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-    return [`${days} 天 ${hours} 时 ${minutes} 分 ${seconds} 秒`, true];
+    return [`${days} 天 ${hours} 时 ${minutes} 分 ${seconds} 秒`, false];
 
 }
 
@@ -42,27 +50,27 @@ function App() {
     const [leftDays, setLeftDays] = useState('');
 
     useEffect(() => {
-
-
-
-
-        // 创建一个定时器，每秒更新一次数据
-        const interval = setInterval(() => {
-            // 计算剩余工作天数
-            const days = calcLeftWorkDays();
-            setLeftWorkDays(days);
-            // 计算剩余总天数
+        const interval1 = setInterval(() => {
             const [daysString, isEnd] = calcLeftDays();
             if (isEnd) {
-                clearInterval(interval);
+                clearInterval(interval1);
                 setLeftDays("0")
                 return
             }
             setLeftDays(daysString);
         }, 1000);
 
-        // 在组件卸载时清理定时器
-        return () => clearInterval(interval);
+        const days = calcLeftWorkDays();
+        setLeftWorkDays(days);
+        const interval2 = setInterval(() => {
+            const days = calcLeftWorkDays();
+            setLeftWorkDays(days);
+        }, 1000*60*60);
+
+        return ()=> {
+            clearInterval(interval1);
+            clearInterval(interval2);
+        };
     }, []);
 
     return (
